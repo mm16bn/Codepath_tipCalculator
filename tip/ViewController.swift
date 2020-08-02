e2e2a1cdd99b54b7841dc77d078d0aee879b3e06
mm16bn <32907153+mm16bn@users.noreply.github.com>
2020-08-02 15:51:29 -0400

    Add keyboard opens automatically

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+import Foundation
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    private var locationManager:CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+//        getUserLocation()
+        billAmountTextField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+    }
 
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // Get bill amount
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        // Calculate tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        // Update tip and total labels
+        tipPercentageLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+    }
+    
+    func getUserLocation(){
+        locationManager = CLLocationManager()
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.startUpdatingLocation()
+    }
 }
 
